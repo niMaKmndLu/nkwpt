9d44410de6d6eab9bddfca799e90c4f73c91d6de
niMa <nima.kmndlu@gmail.com>
2020-04-01 20:54:10 +0430

    practis with hooks

--- functions.php
+++ functions.php
@@ -1,1 +1,23 @@
 <?php
+
+function nima_func($id){
+	echo "logged id is {$id}";
+}
+
+function nima_func2($id){
+	$idd = $id + 10;
+	echo "<br> and its my parent {$idd}";
+}
+
+class User{
+	static public function Login($id){
+		echo "logged id is {$id}";
+	}
+}
+
+add_action('nkwpt', 'User::Login');
+add_action('nkwpt', 'nima_func2');
+
+$id = 10;
+
+do_action('nkwpt',$id);
