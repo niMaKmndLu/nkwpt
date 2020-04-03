e6347edd47290071194bb176f94bcb44e484c523
niMa <nima.kmndlu@gmail.com>
2020-04-03 12:31:12 +0430

    try filter hook

--- functions.php
+++ functions.php
@@ -1,23 +1,28 @@
 <?php
 
-function nima_func($id){
-	echo "logged id is {$id}";
+
+//$black_list = [1,2,3];
+//
+//function nk_add_user_to_black_list($users){
+//	$users[] = 23;
+//
+//	return $users;
+//}
+//
+//add_filter('add_user_to_black_list','nk_add_user_to_black_list');
+//
+//$black_list = apply_filters('add_user_to_black_list', $black_list);
+//
+//var_dump($black_list);
+
+$content = 'وردپرس یک سیستم مدیریت محتوای رایگان میباشد. وردپرس خیلییی خوبه.';
+
+function nk_filter_wordpress_word($content){
+	$content = preg_replace('/وردپرس/', '<a href="/category/wordpress">وردپرس</a>', $content);
+	return $content;
 }
 
-function nima_func2($id){
-	$idd = $id + 10;
-	echo "<br> and its my parent {$idd}";
-}
+add_filter('custom_content', 'nk_filter_wordpress_word');
+$content = apply_filters('custom_content',$content);
 
-class User{
-	static public function Login($id){
-		echo "logged id is {$id}";
-	}
-}
-
-add_action('nkwpt', 'User::Login');
-add_action('nkwpt', 'nima_func2');
-
-$id = 10;
-
-do_action('nkwpt',$id);
+echo '<p>'.$content.'</p>';
